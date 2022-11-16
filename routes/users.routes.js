d7a66f0dfac5b8e337d74d5f9ed27eaa2e916c32
phomit03 <thaonxth2108014@fpt.edu.vn>
2022-11-16 23:46:54 +0700

    up code

---
+++ routes/users.routes.js
@@ -0,0 +1,16 @@
+module.exports = app => {
+    const users = require("../controllers/users.controller");
+
+    var router = require("express").Router();
+
+    //home
+    router.get("/", users.views);
+
+    //get
+    router.get("/getUser", users.getAllUser);
+
+    router.get("/addUser", users.formUser);
+    router.post("/addUser", users.addUser);
+
+    app.use('/api/users', router);
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./models")
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.set('view engine', 'ejs');
+app.use('views', express.static('views'))
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+})
+require('./routes/users.routes')(app);
+
+db.sequelize.sync();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
